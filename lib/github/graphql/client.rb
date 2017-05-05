139837d0127c640f3bfc0566b44a756a2d02211b
takkanm <takkanm@gmail.com>
2017-05-06 08:16:51 +0900

    use inheritance

--- lib/github/graphql/client.rb
+++ lib/github/graphql/client.rb
@@ -7,10 +7,19 @@ module GitHub
     URL = 'https://api.github.com/graphql'.freeze
 
     class Client
-      def initialize(token: )
-        http = ::GraphQL::Client::HTTP.new(GitHub::GraphQL::URL) do
-          define_method :headers, ->(context) { context.merge({"Authorization" => "bearer #{token}"}) }
+      class HTTP < ::GraphQL::Client::HTTP
+        def initialize(token)
+          @token = token
+          super(GitHub::GraphQL::URL)
         end
+
+        def headers(context)
+          context.merge({"Authorization" => "bearer #{@token}"})
+        end
+      end
+
+      def initialize(token: )
+        http   = ::GitHub::GraphQL::Client::HTTP.new(token)
         schema = ::GraphQL::Client.load_schema(http)
 
         @client = ::GraphQL::Client.new(schema: schema, execute: http)
